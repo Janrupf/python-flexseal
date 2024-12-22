c5e33840d07e295f80b202ab34c9f280e1aec957
Janrupf <business.janrupf@gmail.com>
2024-12-22 03:48:32 +0100

    Make output stable

--- flexseal/__main__.py
+++ flexseal/__main__.py
@@ -3,7 +3,29 @@ from flexseal.pip.install_report import InstallReport
 from flexseal.pip.models import InstallReportModel
 from flexseal.sealed import SealedInstallInstructions
 import argparse
+import json
 
+# Hacks hacks hacks... make the output stable
+def dict_sort_key(v):
+    if isinstance(v, dict):
+        if "name" in v:
+            return v["name"]
+
+        raise ValueError(f"Cannot sort dict {v}")
+
+    return v
+
+def order_json_obj(obj):
+    if isinstance(obj, dict):
+        result = {}
+        for k, v in sorted(obj.items()):
+            result[k] = order_json_obj(v)
+
+        return result
+    if isinstance(obj, list):
+        return sorted([order_json_obj(x) for x in obj], key=dict_sort_key)
+
+    return obj
 
 def main():
     parser = argparse.ArgumentParser(
@@ -60,8 +82,12 @@ def main():
         packages=sealed_required_packages
     )
 
+    # Not pretty, but it works - sort the json so the output is deterministic
+    # (and without having to think about that EVERYWHERE)
+    raw_obj = order_json_obj(json.loads(sealed_instructions.model_dump_json()))
+
     with open(output_file, "w") as instructions_file:
-        instructions_file.write(sealed_instructions.model_dump_json(indent=2))
+        json.dump(raw_obj, instructions_file, indent=2, sort_keys=True)
 
 
 if __name__ == "__main__":
